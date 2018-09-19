125cf491ee331382747f4eb78f3340bf4959868f
Nikita Vysotskiy <nikita.vysotskiy@io-soft.com.ua>
2018-09-19 23:41:51 +0300

    icoholder init

--- crypto/crypto/spiders/icoholder.py
+++ crypto/crypto/spiders/icoholder.py
@@ -6,19 +6,19 @@ from ..utils import xpath_exract_first_text, parse_social_link, xpath_tolerant
 
 
 XPATH_SOCIAL_LINK = ''
-XPATH_TITLE = ''
+XPATH_TITLE = '//div[contains(@class, "logo-name")]//h1'
 
 
 class IcoholderSpider(scrapy.Spider):
     name = "icoholder"
 
     def start_requests(self):
-        urls = ('',)
+        urls = ('https://icoholder.com/en/icos/all',)
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        next_pages = response.xpath('').extract()
+        next_pages = response.xpath('//*[@id="features"]/div/div/div/div/div/div/div/a/@href').extract()
 
         for next_page in next_pages:
             yield response.follow(next_page, callback=self.parse_ico)
