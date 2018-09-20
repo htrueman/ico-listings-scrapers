e71b682ed04694b020485f2ca468071e4d5f7d71
Nikita Vysotskiy <nikita.vysotskiy@io-soft.com.ua>
2018-09-20 10:21:55 +0300

    trackico members

--- crypto/crypto/spiders/teams/trackico.py
+++ crypto/crypto/spiders/teams/trackico.py
@@ -3,13 +3,14 @@ import scrapy
 from ...utils import xpath_exract_first_text
 
 XPATH_TITLE = '//*[@class="main-container"]//h1[@class="h2"]'
-XPATH_MEMBER_LINKS = ''
+XPATH_MEMBER = '//*[@id="tab-team"]//div[contains(@class, "card-body")]'
+XPATH_LINKEDIN_LINK = '//a[contains(@href, "linkedin.com")]/@href'
 
-MAX_PAGE = 1
+MAX_PAGE = 163
 
 
 class TrackicoSpider(scrapy.Spider):
-    name = "trackico"
+    name = "trackico_members"
 
     def start_requests(self):
         urls = ('https://www.trackico.io/{}/'.format(i) for i in range(1, MAX_PAGE + 1))
@@ -24,21 +25,27 @@ class TrackicoSpider(scrapy.Spider):
             yield response.follow(next_page, callback=self.parse_ico)
 
     def parse_ico(self, response):
-        top_3_members = response.xpath(XPATH_MEMBER_LINKS).extract()[:3]
+        ico_title = xpath_exract_first_text(response, XPATH_TITLE)
 
-        for member in top_3_members:
-            next_page = ''
+        members_links = response.xpath(XPATH_MEMBER + '/h5/a/@href').extract()[:3]
+        members_names = response.xpath(XPATH_MEMBER + '/h5/a/text()').extract()[:3]
+        members_postions = response.xpath(XPATH_MEMBER + '/span[text() != "\n"]/text()').extract()[:3]
+
+        for link, name, position in zip(members_links, members_names, members_postions):
             yield response.follow(
-                next_page,
+                link,
                 callback=self.parse_member,
-                meta={'title': xpath_exract_first_text(response, XPATH_TITLE)}
+                meta={
+                    'ico_title': ico_title,
+                    'member_name': name.replace('\n', '').lower(),
+                    'member_position': position
+                }
             )
 
     def parse_member(self, response):
-
         yield {
-            'ico_title': '',
-            'member_name': '',
-            'member_position': '',
-            'member_linkedin_link': '',
+            'ico_title': response.meta['ico_title'],
+            'member_name': response.meta['member_name'],
+            'member_position': response.meta['member_position'],
+            'member_linkedin_link': response.xpath(XPATH_LINKEDIN_LINK).extract_first(),
         }
