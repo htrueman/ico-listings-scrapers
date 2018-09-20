1fbcca83be9c2509fce34eec6e3e96e49d6396ec
Nikita Vysotskiy <nikita.vysotskiy@io-soft.com.ua>
2018-09-20 07:55:29 +0300

    trackico fixes

--- crypto/crypto/spiders/trackico.py
+++ crypto/crypto/spiders/trackico.py
@@ -4,26 +4,26 @@ import scrapy
 
 from ..utils import xpath_tolerant, xpath_exract_first_text, parse_social_link
 
-XPATH_SOCIAL_LINK = '/html/body/main/div/div[1]/div[1]/div/div[2]/div/div/div[1]/div[2]/div[2]/a[@href[contains(., "{href_contains}")]]/@href'
-XPATH_BOUNTY_LINK = '/html/body/main/div/div[1]/div[1]/div/div[2]/div/div/div[1]/div[2]/div[2]/a[@href[contains(., "bitcointalk.org")]]/@href'
-XPATH_TITLE = '/html/body/main/div/div[1]/div[1]/div/div[2]/div/div/div[1]/div[2]/div[1]/h1'
-XPATH_RATING = '/html/body/main/div/div[1]/div[2]/div/div[2]/div/div/div/strong'
-XPATH_WEBSITE = '/html/body/main/div/div[1]/div[1]/div/div[2]/div/div/div[1]/div[2]/div[2]/a[text()[contains(., "Website")]]/@href'
-XPATH_WHITEPAPER = '/html/body/main/div/div[1]/div[1]/div/div[2]/div/div/div[1]/div[2]/div[2]/a[text()[contains(., "Whitepaper")]]/@href'
-XPATH_PRE_SALE = '/html/body/main/div/div[1]/div[2]/div/div[3]/div/table/tbody/tr[./th[contains(., "Pre-Sale")]]/child::td[1]'
-XPATH_TOKEN_SALE = '/html/body/main/div/div[1]/div[2]/div/div[3]/div/table/tbody/tr[./th[contains(., "Token Sale")]]/child::td[1]'
-XPATH_COUNTRY = '/html/body/main/div/div[1]/div[2]/div/div[3]/div/table/tbody/tr[./th[contains(., "Country")]]/child::td[1]/a'
-XPATH_PLATFORM = '//*[@id="tab-financial"]/div/div[1]/div/div/div/table/tbody/tr[./th[contains(., "Platform")]]/child::td[1]/a'
-XPATH_TOKEN_PRICE = '//*[@id="tab-financial"]/div/div[1]/div/div/div/table/tbody/tr[./th[contains(., "Token Price")]]/child::td[1]'
-XPATH_TOKEN_FOR_SALE = '//*[@id="tab-financial"]/div/div[1]/div/div/div/table/tbody/tr[./th[contains(., "Token for sale")]]/child::td[1]'
-XPATH_TOKEN_SUPPLY = '//*[@id="tab-financial"]/div/div[1]/div/div/div/table/tbody/tr[./th[contains(., "Token supply")]]/child::td[1]'
-XPATH_SOFT_CAP = '//*[@id="tab-financial"]/div/div[2]/div/div/div/table/tbody/tr[./th[contains(., "Soft cap")]]/child::td[1]'
-XPATH_HARD_CAP = '//*[@id="tab-financial"]/div/div[2]/div/div/div/table/tbody/tr[./th[contains(., "Hard cap")]]/child::td[1]'
-XPATH_ACCEPTING = '//*[@id="tab-financial"]/div/div[2]/div/div/div/table/tbody/tr[./th[contains(., "Accepting")]]/child::td[1]'
-XPATH_RESTRICTED_COUNTRIES = '//*[@id="tab-financial"]/div/div[2]/div/div/div/table/tbody/tr[./th[contains(., "Restricted countries")]]/child::td[1]'
-XPATH_KYC = '//*[@id="tab-financial"]/div/div[2]/div/div/div/table/tbody/tr[./th[contains(., "Know Your Customer")]]/child::td[1]'
-XPATH_WHITELIST = '//*[@id="tab-financial"]/div/div[2]/div/div/div/table/tbody/tr[./th[contains(., "Whitelist")]]/child::td[1]'
-XPATH_BONUS = '//*[@id="tab-financial"]/div/div[2]/div/div/div/table/tbody/tr[./th[contains(., "Bonus")]]/child::td[1]/p/text()'
+XPATH_SOCIAL_LINK = '//*[@class="main-container"]//a[@href[contains(., "{href_contains}")]]/@href'
+
+XPATH_TITLE = '//*[@class="main-container"]//h1[@class="h2"]'
+XPATH_RATING = '//*[@class="main-container"]//div[footer[contains(., "rating")]]//strong'
+XPATH_WEBSITE = '//*[@class="main-container"]//a[text()[contains(., "Website")]]/@href'
+XPATH_WHITEPAPER = '//*[@class="main-container"]//a[text()[contains(., "Whitepaper")]]/@href'
+XPATH_PRE_SALE = '//*[@class="main-container"]//table/tbody/tr[./th[contains(., "Pre-Sale")]]/child::td[1]'
+XPATH_TOKEN_SALE = '//*[@class="main-container"]//table/tbody/tr[./th[contains(., "Token Sale")]]/child::td[1]'
+XPATH_COUNTRY = '//*[@class="main-container"]//table/tbody/tr[./th[contains(., "Country")]]/child::td[1]/a'
+XPATH_PLATFORM = '//*[@id="tab-financial"]//table/tbody/tr[./th[contains(., "Platform")]]/child::td[1]/a'
+XPATH_TOKEN_PRICE = '//*[@id="tab-financial"]//table/tbody/tr[./th[contains(., "Token Price")]]/child::td[1]'
+XPATH_TOKEN_FOR_SALE = '//*[@id="tab-financial"]//table/tbody/tr[./th[contains(., "Token for sale")]]/child::td[1]'
+XPATH_TOKEN_SUPPLY = '//*[@id="tab-financial"]//table/tbody/tr[./th[contains(., "Token supply")]]/child::td[1]'
+XPATH_SOFT_CAP = '//*[@id="tab-financial"]//table/tbody/tr[./th[contains(., "Soft cap")]]/child::td[1]'
+XPATH_HARD_CAP = '//*[@id="tab-financial"]//table/tbody/tr[./th[contains(., "Hard cap")]]/child::td[1]'
+XPATH_ACCEPTING = '//*[@id="tab-financial"]//table/tbody/tr[./th[contains(., "Accepting")]]/child::td[1]'
+XPATH_RESTRICTED_COUNTRIES = '//*[@id="tab-financial"]//table/tbody/tr[./th[contains(., "Restricted countries")]]/child::td[1]'
+XPATH_KYC = '//*[@id="tab-financial"]//table/tbody/tr[./th[contains(., "Know Your Customer")]]/child::td[1]'
+XPATH_WHITELIST = '//*[@id="tab-financial"]//table/tbody/tr[./th[contains(., "Whitelist")]]/child::td[1]'
+XPATH_BONUS = '//*[@id="tab-financial"]//table/tbody/tr[./th[contains(., "Bonus")]]/child::td[1]/p/text()'
 
 MAX_PAGE = 163
 
@@ -48,7 +48,7 @@ class TrackicoSpider(scrapy.Spider):
     def parse_ico(self, response):
 
         try:
-            bounty_link = response.xpath(XPATH_BOUNTY_LINK).extract()[1]
+            bounty_link = response.xpath(XPATH_SOCIAL_LINK.format(href_contains="bitcointalk.org")).extract()[1]
         except Exception:
             bounty_link = None
 
