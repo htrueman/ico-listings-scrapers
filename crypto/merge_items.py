30ee5d97414efc20d8cdb7a9c94630d5d4ec7850
Vadym Hevlich <vege1wgw@gmail.com>
2018-09-23 21:02:35 +0300

    merge items v2.7

--- crypto/merge_items.py
+++ crypto/merge_items.py
@@ -17,13 +17,13 @@ json_orgs = json.loads(imported_organizations.export('json'))
 for i in range(parts_count):
     range_start = i * len(json_orgs) // parts_count
     range_end = ((i + 1) * len(json_orgs)) // parts_count
-    with open('imported_organizations_spited_{}.json'.format(i), 'w+') as f:
+    with open('imported_organizations_splitted_{}.json'.format(i), 'w+') as f:
         f.write(json.dumps(json_orgs[range_start:range_end]))
 
 
 with open(members_file_name, 'w+') as ndm_file:
     for i in range(parts_count):
-        part_file_name = 'imported_organizations_spited_{}.json'.format(i)
+        part_file_name = 'imported_organizations_splitted_{}.json'.format(i)
         with open(part_file_name, 'r') as f1:
             content = f1.read()
             imported_organizations = json.loads(content)
@@ -44,15 +44,15 @@ with open(members_file_name, 'w+') as ndm_file:
                                 if 'link' in content_key.lower():
                                     if organization[content_key] and \
                                             organization[content_key].lower() in content_value.lower():
-                                        organization[content_key] = organization[content_key] \
-                                            if len(organization[content_key]) >= len(content_value) else content_value
 
                                         for member in imported_members_json:
                                             if member['Organization'] in [organization['Name'], ndo_organization['Name']]:
                                                 member['Organization'] = organization['Name']
-
-                                        ndo_file.write(json.dumps(organization) + ',')
                                         to_write = False
+                                if not to_write:
+                                    organization.update({k: v for k, v in ndo_organization.items()
+                                                         if len(v) > len(organization[k])})
+                                    ndo_file.write(json.dumps(organization) + ',')
                         if to_write:
                             ndo_file.write(json.dumps(organization) + ',')
     ndm_file.write(imported_members)
