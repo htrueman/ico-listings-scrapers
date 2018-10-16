7bc9702d4a3b32923287782f1f13d6b842b637a2
Vadym Hevlich <vege1wgw@gmail.com>
2018-10-16 14:34:37 +0300

    merge items fix

--- utils/merge_items.py
+++ utils/merge_items.py
@@ -9,56 +9,55 @@ from constants import OrgFields
 
 class MergeItems:
     def __init__(self, imported_orgs_file_name=None, imported_members_file_name=None, *args, **kwargs):
-        if not imported_orgs_file_name or not imported_members_file_name:
-            imported_members_file_name = '/home/hevlich/Projects/crypto/files_to_import/people.csv'
+        # if not imported_orgs_file_name or not imported_members_file_name:
+        #     imported_members_file_name = '/Users/imac/Projects/crypto/people.csv'
 
         self.imported_organizations = tablib.Dataset(json.loads(open(imported_orgs_file_name).read()))
-        self.imported_members = tablib.Dataset().load(open(imported_members_file_name).read())
+        # self.imported_members = tablib.Dataset().load(open(imported_members_file_name).read())
         self.organizations_file_name = 'non_duplicate_{}.json'.format(imported_orgs_file_name)
-        self.members_file_name = 'non_duplicate_people.json'
+        # self.members_file_name = 'non_duplicate_people.json'
 
-        with open(self.members_file_name, 'w+') as f:
-            f.write('')
-
-        self.members_file = open(self.members_file_name, 'r+')
-        self.members_file.write('[')
+        # with open(self.members_file_name, 'w+') as f:
+        #     f.write('')
+        #
+        # self.members_file = open(self.members_file_name, 'r+')
+        # self.members_file.write('[')
 
         self.ndo_content = []
         self.make_merge()
 
-    @staticmethod
-    def find_related_members(imported_members_json, name1, name2=None, final_name=None):
-        members = []
-        for member in imported_members_json:
-            if member['Organization'] in [name1, name2]:
-                member['Organization'] = final_name or name1
-                members.append(member)
-                imported_members_json.remove(member)
+    # @staticmethod
+    # def find_related_members(imported_members_json, name1, name2=None, final_name=None):
+    #     members = []
+    #     for member in imported_members_json:
+    #         if member['Organization'] in [name1, name2]:
+    #             member['Organization'] = final_name or name1
+    #             members.append(member)
+    #             imported_members_json.remove(member)
+    #
+    #     unique_members = []
+    #     while len(members):
+    #         name_key = 'Linkedin link'
+    #         link_key = 'Name'
+    #         current = members.pop()
+    #         rest_links = [m[link_key] for m in members]
+    #         rest_names = [m[name_key] for m in members]
+    #         if current[name_key] \
+    #                 and current[name_key] in rest_names \
+    #                 or current[link_key] \
+    #                 and current[link_key] in rest_links:
+    #             pass
+    #         else:
+    #             unique_members.append(current)
+    #
+    #     return unique_members, imported_members_json
+    #
+    # @staticmethod
+    # def write_members(members, members_file):
+    #     if members:
+    #         members_file.write(json.dumps(members)[1:-1] + ',')
 
-        unique_members = []
-        while len(members):
-            name_key = 'Linkedin link'
-            link_key = 'Name'
-            current = members.pop()
-            rest_links = [m[link_key] for m in members]
-            rest_names = [m[name_key] for m in members]
-            if current[name_key] \
-                    and current[name_key] in rest_names \
-                    or current[link_key] \
-                    and current[link_key] in rest_links:
-                pass
-            else:
-                unique_members.append(current)
-
-        return unique_members, imported_members_json
-
-    @staticmethod
-    def write_members(members, members_file):
-        if members:
-            members_file.write(json.dumps(members)[1:-1] + ',')
-
-    def merge_main(self, organization, mergeable_link_keys,
-                   merged_count, imported_members_json):
+    def merge_main(self, organization, mergeable_link_keys, merged_count):
         merge = False
         for ndo_index, ndo_organization in enumerate(self.ndo_content):
             for content_key, content_value in ndo_organization.items():
@@ -83,12 +82,12 @@ class MergeItems:
                     merged_count += 1
 
                     final_name = organization['name']
-                    members, imported_members_json = self.find_related_members(
-                        imported_members_json,
-                        organization_name,
-                        ndo_organisation_name,
-                        final_name)
-                    self.write_members(members, self.members_file)
+                    # members, imported_members_json = self.find_related_members(
+                    #     imported_members_json,
+                    #     organization_name,
+                    #     ndo_organisation_name,
+                    #     final_name)
+                    # self.write_members(members, self.members_file)
                     print('merged: ', merged_count)
 
                     break
@@ -102,7 +101,7 @@ class MergeItems:
             # self.write_members(members, self.members_file)
 
     def make_merge(self):
-        imported_members_json = json.loads(self.imported_members.export('json'))
+        # imported_members_json = json.loads(self.imported_members.export('json'))
         imported_organizations_json = json.loads(self.imported_organizations.export('json'))[0]
         merged_count = 0
 
@@ -117,14 +116,13 @@ class MergeItems:
             if index == 0:
                 self.ndo_content.append(organization)
             else:
-                self.merge_main(organization, mergeable_link_keys,
-                                merged_count, imported_members_json)
+                self.merge_main(organization, mergeable_link_keys, merged_count)
 
         with open(self.organizations_file_name, 'w') as f:
             f.write(json.dumps(self.ndo_content))
 
-        self.members_file.write(']')
-        self.members_file.close()
+        # self.members_file.write(']')
+        # self.members_file.close()
 
 
 if __name__ == '__main__':
